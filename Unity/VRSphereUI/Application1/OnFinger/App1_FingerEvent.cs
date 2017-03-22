c55b04a3bae67f207a2220c54532fe38b95165b2
ric113 <ric1134991@gmail.com>
2017-03-22 15:08:55 +0800

    APP1:修正角度問題,完成照片也一起透明

--- Unity/VRSphereUI/Application1/OnFinger/App1_FingerEvent.cs
+++ Unity/VRSphereUI/Application1/OnFinger/App1_FingerEvent.cs
@@ -25,23 +25,28 @@ public class App1_FingerEvent : MonoBehaviour {
 	void OnCollisionEnter(Collision collision)
 	{
 		/* 使surface1透明 , surface2顯現 */
-        if (collision.gameObject.tag == "middleRegion") 
-		{
-			surfaceOneRenderer.material
-				.SetColor ("_Color", new Color(surfaceOneRenderer.material.color.r,surfaceOneRenderer.material.color.g,surfaceOneRenderer.material.color.b,0.1f));
+         if (collision.gameObject.tag == "middleRegion") 
+        {
+            surfaceOneRenderer.material
+                .SetColor ("_Color", new Color(surfaceOneRenderer.material.color.r,surfaceOneRenderer.material.color.g,surfaceOneRenderer.material.color.b,0.1f));
+            setObjOnSurfaceAlpha (surfaceOne, 0.1f);
 
-			surfaceTwoRenderer.material
-				.SetColor ("_Color", new Color(surfaceTwoRenderer.material.color.r,surfaceTwoRenderer.material.color.g,surfaceTwoRenderer.material.color.b,0.3f));
-		}
+            surfaceTwoRenderer.material
+                .SetColor ("_Color", new Color(surfaceTwoRenderer.material.color.r,surfaceTwoRenderer.material.color.g,surfaceTwoRenderer.material.color.b,0.3f));
+            setObjOnSurfaceAlpha (surfaceTwo, 0.3f);
+        }
 
-		/* (surface1 init時是顯現)使surface1顯現 , surface2透明 */
+        /* (surface1 init時是顯現)使surface1顯現 , surface2透明 */
         if (collision.gameObject.tag == "surface1") 
-		{
-			surfaceOneRenderer.material
-				.SetColor ("_Color", new Color(surfaceOneRenderer.material.color.r,surfaceOneRenderer.material.color.g,surfaceOneRenderer.material.color.b,0.3f));
-			surfaceTwoRenderer.material
-				.SetColor ("_Color", new Color(surfaceTwoRenderer.material.color.r,surfaceTwoRenderer.material.color.g,surfaceTwoRenderer.material.color.b,0.1f));
-		}
+        {
+            surfaceOneRenderer.material
+                .SetColor ("_Color", new Color(surfaceOneRenderer.material.color.r,surfaceOneRenderer.material.color.g,surfaceOneRenderer.material.color.b,0.3f));
+            setObjOnSurfaceAlpha (surfaceOne, 0.3f);
+
+            surfaceTwoRenderer.material
+                .SetColor ("_Color", new Color(surfaceTwoRenderer.material.color.r,surfaceTwoRenderer.material.color.g,surfaceTwoRenderer.material.color.b,0.1f));
+            setObjOnSurfaceAlpha (surfaceTwo, 0.1f);
+        }
 
         /* Object放置在surface2上 */
         if (collision.gameObject.tag == "surface2")
@@ -74,20 +79,23 @@ public class App1_FingerEvent : MonoBehaviour {
     void getTouchPointAngel()
     {
         Vector3 touchPointVector = surface2TouchPoint.transform.localPosition;
-        Vector3 projectToZ = new Vector3(touchPointVector.x, touchPointVector.y, 0f);
+        //Vector3 projectToZ = new Vector3(touchPointVector.x, touchPointVector.y, 0f);
         Vector3 projectToY = new Vector3(touchPointVector.x, 0f, touchPointVector.z);
-        bool verticalUp = (touchPointVector.y >= 0); /* -90 ~ 90 */
+        //bool verticalUp = (touchPointVector.y >= 0); /* -90 ~ 90 */
         bool horizontalRight = (touchPointVector.z >= 0); /* -180 ~ -180 */
 
+        /*
         verticalAngel = (projectToZ == Vector3.zero) ? 0f : Vector3.Angle(projectToZ, surfaceTwo.transform.right);
         if (verticalAngel > 90)
             verticalAngel = 180 - verticalAngel;
         if (!verticalUp)
             verticalAngel = -1 * verticalAngel;
+        */
+       
+       verticalAngel = Vector3.Angel(touchPointVector, -1 * surfaceTwo.transform.up);
+       verticalAngel -= 90;
 
         horizontalAngel = (projectToY == Vector3.zero) ? 0f : Vector3.Angle(projectToY, surfaceTwo.transform.right);
-        if (horizontalAngel > 90)
-            horizontalAngel = 180 - horizontalAngel;
         if (horizontalRight)
             horizontalAngel = -1 * horizontalAngel;
 
@@ -98,5 +106,21 @@ public class App1_FingerEvent : MonoBehaviour {
         onHandObj.GetComponent<App1_ButtonTouchEvent>().setButtonOnSurface2(verticalAngel,horizontalAngel);
         onHandObj = null;
         Destroy(surface2TouchPoint);
+    }
+
+    void setObjOnSurfaceAlpha(GameObject surface ,float alpha){
+
+        Renderer onSurfaceObjRenderer;
+        foreach (Transform child in surface.transform)
+        {
+            //child is your child transform
+            foreach(Transform _child in child.transform){
+                if (_child.tag == "onSurfaceObj") {
+                    onSurfaceObjRenderer = child.GetComponentInChildren<Renderer> ();
+                    onSurfaceObjRenderer.material.SetColor ("_Color", new Color (onSurfaceObjRenderer.material.color.r, onSurfaceObjRenderer.material.color.g, onSurfaceObjRenderer.material.color.b, alpha));
+                }
+            }
+        }
+
     }
 }
